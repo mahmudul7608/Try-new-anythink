71ce0f03a1730cf9d5fb5406181d0c31ac4e56fd
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-12-03 18:23:14 +0600

    first commit

---
+++ scrit.js
@@ -0,0 +1,12 @@
+// === Togol nav ===
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// === Togol nav end ===
+
